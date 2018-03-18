56633d453be02132a51d5877ed15c6d050791d94
funnyjoshua <funnyjoshua@JOSHUA>
2018-03-17 23:37:21 -0600

    Add project files.

---
+++ FirstTextRPG/GameStart.cs
@@ -0,0 +1,53 @@
+﻿using FirstTextRPG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTextRPG
+{
+
+    /*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    * 
+    * This is for the start of the game
+    * This will ask the user if they want to start a new game
+    * If yes, let them make a new game
+    * If no, ask if they want to load an existing save
+    * Else, tell them to do yes or no
+    *
+    *//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    class GameStart
+    {
+        //Text for intro, this asks if they want to start a new game
+        public String introText = "Welcome, would you like to start a new game? Y/N"; //basic question
+        public Boolean introChecked = false; //need this to reask after false answers
+
+        public void introAnswerChecker(String input)
+        {
+            if (input == "y" || input == "Y") //checks for yes
+            {
+                //TESTLINE
+                Console.WriteLine("YES HAS BEEN ENTERED");
+                introChecked = true;
+            } //checks for yes
+            else if (input == "n" || input == "N") //Checks for no
+            {
+                //TESTLINE
+                Console.WriteLine("NO HAS BEEN ENTERED");
+                //line commented because save games arent in yet, so hitting no would act as yes until this is implemented
+                //introChecked = true;
+                Console.ReadKey(); //Hold an input so user can read
+                Console.Clear(); //Clear console
+            } //Checks for no
+            else //checks for invalid inputs
+            {
+                //Tells user their input was invalid
+                Console.WriteLine("Invalid Input, try Y or N");
+                Console.ReadKey(); //Hold an input so user can read
+                Console.Clear(); //Clear console
+            } //checks for invalid inputs
+        } //public void introAnswerChecker(String input)
+    } //class GameStart
+} //namespace FirstTextRPG
+
---
+++ FirstTextRPG/Program.cs
@@ -0,0 +1,36 @@
+﻿using FirstTextRPG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTextRPG
+{
+    /*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     * 
+     * My text based RPG
+     * Goals: Inventory System, Combat, Go area to area, Random encounters, Save system, 
+     * Make it pretty much like Corruption of Champions for now, minus the sex and shite inventory system
+     *
+     *//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            //instnaciate classes
+            GameStart gameStart = new GameStart(); 
+
+            //intro to game, ask if they want new game or existing save
+            while(gameStart.introChecked == false)
+            {
+                Console.WriteLine(gameStart.introText); //Asks player if they want to start a new game
+
+                gameStart.introAnswerChecker(Console.ReadLine()); //Check for response
+            }
+                Console.ReadKey(); //makes user hit any key to exit
+        }
+    }
+}
